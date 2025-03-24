1b8b44b32c5eac2bf6ecf08bab44ef28e98826a9
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-03-24 10:49:29 +0700

    nambahin operasi

--- calc.cpp
+++ calc.cpp
@@ -20,6 +20,29 @@ int main(){
         cout << "pilih operasi: ";
         cin >> aritmatika;
 
+        //proses
+        switch(aritmatika){
+            case '*':
+                hasil = a * b;
+                cout << "hasil dari " << a << " dikali " << b << " adalah " << hasil << endl;
+                break;
+            case '/':
+                hasil = a / b;
+                cout << "hasil dari " << a << " dibagi " << b << " adalah " << hasil << endl;
+                break;
+            case '+':
+                hasil = a + b;
+                cout << "hasil dari " << a << " ditambah " << b << " adalah " << hasil << endl;
+                break;
+            case '-':
+                hasil = a - b;
+                cout << "hasil dari " << a << " dikurang " << b << " adalah " << hasil << endl;
+                break;
+            default:
+                cout << "inputanmu ada yang salah!" << endl;
+        }
+
+        
         //looping
         cout << "Ingin menggunakannya lagi?" << endl;
         cin >> pilih;
